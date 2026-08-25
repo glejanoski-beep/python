4b74be81be7b12f9cb99201929e9c3698ac83683
glejanoski-beep <guilherme.lejanoski@aluno.impacta.edu.br>
2026-08-24 21:58:43 -0300

    Inclusão exercicios 16 e 17

---
+++ exercicios_1ao100/exercicio16.py
@@ -0,0 +1,26 @@
+num = int(input("Digite um valor: "))
+
+if num > 0:
+    print("POSITIVO")
+else: 
+    if num < 0:
+             print("NEGATIVO")
+    else:
+            if num == 0:
+                    print("ZERO")
+   
+
+'''
+TESTE 1 
+Digite um valor: 12
+POSITIVO
+
+TESTE 2 
+Digite um valor: -5
+NEGATIVO
+
+TESTE 3
+Digite um valor: 0
+ZERO
+
+'''
---
+++ exercicios_1ao100/exercicio17.py
@@ -0,0 +1,25 @@
+num = int(input("Digite um valor: "))
+
+resultado = num % 2
+
+if resultado == 0:
+    print(f"Resultado: PAR")
+else:
+    print("ÍMPAR")
+
+
+'''
+TESTE 1
+Digite um valor: 12
+Resultado: PAR
+
+TESTE 2
+Digite um valor: 7
+IMPAR
+
+TESTE 3
+Digite um valor: -8
+Resultado: PAR
+
+
+'''
