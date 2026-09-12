a1feee3d11896a7249dd02dd507b8e537ca6ace8
glejanoski-beep <guilherme.lejanoski@aluno.impacta.edu.br>
2026-09-12 20:30:47 -0300

    funcoes simples (incompleto)

---
+++ Funcoes/funcaoSimpples.py
@@ -0,0 +1,18 @@
+def eh_par(numero):
+    '''
+    Verifica se um numero é par
+    Retorna true se for par, false caso contrário
+    '''
+    return numero % 2 == 0
+
+def eh_impar(numero):
+    ''' 
+    Verifica se o um numero é impar
+    Retorna true se for impar, false caso contrário
+    '''
+    return numero % 2 != 0
+
+def interagir_com_usuario():
+    '''
+    Função principal que interage com o usuário.
+    '''
